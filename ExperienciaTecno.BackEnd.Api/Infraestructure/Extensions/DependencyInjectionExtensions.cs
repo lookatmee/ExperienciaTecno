73367c690617cd61c1dbd64ac9fc67dee5fb0783
lookatmee <adolfojinete@hotmail.com>
2024-07-18 20:36:01 -0500

    feat: create and implemented UnitOfWork

---
+++ ExperienciaTecno.BackEnd.Api/Infraestructure/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,51 @@
+﻿using ExperienciaTecno.BackEnd.Core.Category.Models;
+using ExperienciaTecno.BackEnd.Core.Category.Repositories;
+using ExperienciaTecno.BackEnd.Core.Category.Services;
+using ExperienciaTecno.BackEnd.Core.Category.Services.Impl;
+using ExperienciaTecno.BackEnd.Core.Category.Validators;
+using ExperienciaTecno.BackEnd.Core.Manufacturer.Models;
+using ExperienciaTecno.BackEnd.Core.Manufacturer.Repositories;
+using ExperienciaTecno.BackEnd.Core.Manufacturer.Services;
+using ExperienciaTecno.BackEnd.Core.Manufacturer.Services.Impl;
+using ExperienciaTecno.BackEnd.Core.Manufacturer.Validators;
+using ExperienciaTecno.BackEnd.Core.Product.Models;
+using ExperienciaTecno.BackEnd.Core.Product.Repositories;
+using ExperienciaTecno.BackEnd.Core.Product.Services;
+using ExperienciaTecno.BackEnd.Core.Product.Services.Impl;
+using ExperienciaTecno.BackEnd.Core.Product.Validators;
+using ExperienciaTecno.BackEnd.Data.EF.Repositories;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExperienciaTecno.BackEnd.Api.Infraestructure.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static void RegisterDependencies(this IServiceCollection services)
+    {
+        services.RegisterServices();
+        services.RegisterRepositories();
+        services.RegisterValidators();
+    }
+
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IManufacturerService, ManufacturerService>();
+        services.AddScoped<IProductService, ProductService>();
+    }
+
+    public static void RegisterRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+    }
+
+    public static void RegisterValidators(this IServiceCollection services)
+    {
+        services.AddSingleton<IValidator<Category>, CategoryValidator>();
+        services.AddSingleton<IValidator<Manufacturer>, ManufacturerValidator>();
+        services.AddSingleton<IValidator<Product>,ProductValidator>();
+    }
+}
--- ExperienciaTecno.BackEnd.Api/Program.cs
+++ ExperienciaTecno.BackEnd.Api/Program.cs
@@ -1,9 +1,23 @@
+using ExperienciaTecno.BackEnd.Api.Infraestructure.Extensions;
+using ExperienciaTecno.BackEnd.Core.Common.Data.Impl;
+using ExperienciaTecno.BackEnd.Core.Common.Data;
+using ExperienciaTecno.BackEnd.Data.EF;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+var configuration = builder.Configuration;
+builder.Services.AddDbContext<BackEndDbContext>(option =>
+option.UseSqlServer(configuration.GetConnectionString("ExperienciaTecnoDBConexion")));
+
+builder.Services.RegisterDependencies();
+
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork<BackEndDbContext>>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
